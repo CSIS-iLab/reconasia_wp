dcdbd7735ea43edebc580b6e39065121e9b578d2
Lindsay Urchyk <urchykli@gmail.com>
2021-02-26 12:34:15 -0500

    Add wp debugging to wp config

--- wp-content/themes/reconasia/assets/_js/custom/bundle.js
+++ wp-content/themes/reconasia/assets/_js/custom/bundle.js
@@ -1,9 +1,7 @@
 import { Navigation } from './navbar'
 import { setScrollbarSize } from './scrollbar'
-// import { setGapSupport } from './flex-gap'
 
 document.addEventListener('DOMContentLoaded', function () {
   setScrollbarSize()
   Navigation()
-  // setGapSupport()
 })
