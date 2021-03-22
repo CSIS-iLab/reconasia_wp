8a74f92fb8d11b6c7ea0554d2ee1d29c91cb30eb
jdromero88 <jdromerov88@gmail.com>
2021-03-22 17:00:57 -0400

    Refactor block post to use grid

--- wp-content/themes/reconasia/template-parts/block-post.php
+++ wp-content/themes/reconasia/template-parts/block-post.php
@@ -14,35 +14,24 @@
 ?>
 
 <article <?php post_class('post-block post-block--post'); ?> id="post-<?php the_ID(); ?>">
-	<?php
-		$thumbnail =  has_post_thumbnail();
-	?>
 	<!-- 
 		TODO:
 		- This div should only be added if there's a thumbnail, so it doesn't create space
 		if there isn't an image. Wordpress has a function for this.
 		- Will you please make the thumbnail a link to the post and have the hover effect be applied to the title when you hover over the image?
 	-->
-	<div class="post-block-right">
-		<?php
-			// if ( !is_front_page() ){
-				// echo get_the_post_thumbnail( get_the_ID(), 'large' );		
-			// }
-			if ( $thumbnail ){
-				echo '<a href="' . esc_url( get_permalink() ) .'">';
-					echo get_the_post_thumbnail();
-				'</a>';
-			}
-		?>	
-	</div>
+	
+		<?php if ( has_post_thumbnail() ) : ?>
+			<a href="<?php the_permalink(); ?>" class="post-block__img" title="<?php the_title_attribute(); ?>">
+				<?php the_post_thumbnail( 'large' ); ?>
+			</a>
+		<?php endif; ?>
 
-	<div class="post-block-left">
-		<?php
-			the_title( '<h3 class="post-block__title"><a href="' . esc_url( get_permalink() ) . '">', '</a></h3>' );			
-			the_excerpt();			
-			reconasia_authors();
-			reconasia_posted_on();
-			reconasia_display_categories();
-		?>
-	</div>
+	<?php
+		the_title( '<h3 class="post-block__title"><a href="' . esc_url( get_permalink() ) . '">', '</a></h3>' );
+		the_excerpt();			
+		reconasia_authors();
+		reconasia_posted_on('M j, Y');
+		reconasia_display_categories();
+	?>
 </article><!-- .post -->
