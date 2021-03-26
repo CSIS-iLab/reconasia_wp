2b871166e93ace376bcf4ba24c3195991dbe065c
Jacque Schrag <jnschrag@gmail.com>
2021-03-26 16:48:58 -0400

    Load in featured content

--- wp-content/themes/reconasia/front-page.php
+++ wp-content/themes/reconasia/front-page.php
@@ -13,35 +13,68 @@ get_header();
 ?>
 
 <main id="site-content" role="main">
-	<section class="home__recent">
-		<h2 class="home__recent-section-title">home page <?php echo reconasia_get_svg('doc') ?></h2>
 	<?php
 
-  $args = array(
-    'post_type' => 'post',
-    'post_status' => 'publish',
-    'posts_per_page' => 5
-  );
+	$featured_primary_post = get_field( 'primary_featured_post' );
 
-  $posts = new WP_Query( $args );
+	if ( $featured_primary_post ) :
 
-	if ( $posts->have_posts() ) {
+		echo '<section class="home__featured-secondary">';
 
-		while ( $posts->have_posts() ) {
-			$posts->the_post();
+		foreach ( $featured_primary_post as $post ) :
 
-			if ($posts->current_post === 0) {
-				get_template_part( 'template-parts/block', get_post_type() );
-			} else {
-				get_template_part( 'template-parts/block', get_post_type() );
-			}
+			setup_postdata( $post );
+			get_template_part( 'template-parts/block', get_post_type() );
 
-		}
+		endforeach;
 
 		wp_reset_postdata();
-	}
+		echo '</section>';
+
+	endif;
+
+
+	$featured_secondary_posts = get_field( 'secondary_featured_posts' );
+
+	if ( $featured_secondary_posts ) :
+
+		echo '<section class="home__featured-secondary">';
+
+		foreach ( $featured_secondary_posts as $post ) :
+
+			setup_postdata( $post );
+			get_template_part( 'template-parts/block', get_post_type() );
+
+		endforeach;
+
+		wp_reset_postdata();
+		echo '</section>';
+
+	endif;
 
 	?>
+
+	<section class="home__recent">
+		<h2 class="home__recent-section-title"><?php _e( 'Recent Posts', 'reconasia' ); ?></h2>
+		<?php
+
+		$most_recent_args = array(
+			'post_type' => 'post',
+			'post_status' => 'publish',
+			'posts_per_page' => 5
+		);
+
+		$most_recent_posts = new WP_Query( $most_recent_args );
+
+		if ( $most_recent_posts->have_posts() ) {
+			while ( $most_recent_posts->have_posts() ) {
+				$most_recent_posts->the_post();
+				get_template_part( 'template-parts/block', get_post_type() );
+			}
+			wp_reset_postdata();
+		}
+
+		?>
 	</section>
 
 </main><!-- #site-content -->
