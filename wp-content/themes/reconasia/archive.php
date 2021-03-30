f0bce00a97931c506aa8f48b77256d62845b9e7a
jdromero88 <jdromerov88@gmail.com>
2021-03-30 14:28:12 -0400

    Articles now are inside the main

--- wp-content/themes/reconasia/archive.php
+++ wp-content/themes/reconasia/archive.php
@@ -13,24 +13,60 @@ get_header();
 <main id="site-content" role="main">
 
 	<?php
+		// echo 'hola';
+		// get_template_part( 'template-parts/entry-header' );
 
-		get_template_part( 'template-parts/entry-header' );
+		// if ( have_posts() ) {
 
-		if ( have_posts() ) {
+		// 	reconasia_pagination_number_of_posts();
 
-			reconasia_pagination_number_of_posts();
+		// 	while ( have_posts() ) {
+		// 		the_post();
 
-			while ( have_posts() ) {
-				the_post();
+		// 		get_template_part( 'template-parts/block', get_post_type() );
 
-				get_template_part( 'template-parts/block', get_post_type() );
+		// 	}
+		// 	wp_reset_postdata();
+		// }
 
+		// get_template_part( 'template-parts/pagination' );\
+
+			// get the current taxonomy term
+			$term = get_queried_object();
+			// vars
+			$featured_post = get_field('featured_post', $term);
+			// var_dump($featured_post);
+			if ( $featured_post ) {
+					echo '<div class="post-block--featured">';
+						echo '<span class="post-block--featured__label">Featured</span>';
+						foreach( $featured_post as $post ):
+							// Setup this post for WP functions (variable must be named $post).
+							setup_postdata($post);
+							get_template_part( 'template-parts/block-post-featured' );
+						endforeach;
+					echo '</div>';
+				// Reset the global post object so that the rest of the page works correctly.
+				wp_reset_postdata();
 			}
-			wp_reset_postdata();
-		}
 
-		get_template_part( 'template-parts/pagination' );
-	?>
+			if ( have_posts() ) {
+				$i = 0;
+
+				while ( have_posts() ) {
+					the_post();
+					if ($i == 0) {
+						get_template_part( 'template-parts/block-issues-featured' );
+					} else {
+						get_template_part( 'template-parts/block', get_post_type() );
+					}
+
+					$i++;
+
+				}
+				wp_reset_postdata();
+			}
+
+		?>
 
 </main><!-- #site-content -->
 
--- wp-content/themes/reconasia/index.php
+++ wp-content/themes/reconasia/index.php
@@ -20,63 +20,24 @@ get_header();
 <main id="site-content" role="main">
 
 	<?php
-		if ( is_archive() ) {
-			// get the current taxonomy term
-			$term = get_queried_object();
-			// vars
-			$featured_post = get_field('featured_post', $term);
-			// var_dump($featured_post);
-			if ( $featured_post ) {
-					echo '<div class="post-block--featured">';
-						echo '<span class="post-block--featured__label">Featured</span>';
-						foreach( $featured_post as $post ):
-							// Setup this post for WP functions (variable must be named $post).
-							setup_postdata($post);
-							get_template_part( 'template-parts/block-post-featured' );
-						endforeach;
-					echo '</div>';
-				// Reset the global post object so that the rest of the page works correctly.
-				wp_reset_postdata();
+
+		get_template_part( 'template-parts/entry-header' );
+
+
+		if ( have_posts() ) {
+
+			reconasia_pagination_number_of_posts();
+
+			while ( have_posts() ) {
+				the_post();
+
+				get_template_part( 'template-parts/block', get_post_type() );
+
 			}
-
-			if ( have_posts() ) {
-				$i = 0;
-
-				while ( have_posts() ) {
-					the_post();
-					if ($i == 0) {
-						get_template_part( 'template-parts/block-issues-featured' );
-					} else {
-						get_template_part( 'template-parts/block', get_post_type() );
-					}
-
-					$i++;
-
-				}
-				wp_reset_postdata();
-			}
-
-		} else {
-			get_template_part( 'template-parts/entry-header' );
-
-
-			if ( have_posts() ) {
-
-				reconasia_pagination_number_of_posts();
-
-				while ( have_posts() ) {
-					the_post();
-
-					get_template_part( 'template-parts/block', get_post_type() );
-
-				}
-				wp_reset_postdata();
-			}
-
-
-			get_template_part( 'template-parts/pagination' );
+			wp_reset_postdata();
 		}
-		
+
+	get_template_part( 'template-parts/pagination' );
 	?>
 
 </main><!-- #site-content -->
