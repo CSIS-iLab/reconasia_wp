1439af9b341d0364068bf0009acb4c6d15e7bcda
MrBachChoi <55605733+MrBachChoi@users.noreply.github.com>
2021-04-06 12:36:10 -0400

    working on dropdown

--- wp-content/themes/reconasia/header.php
+++ wp-content/themes/reconasia/header.php
@@ -30,17 +30,21 @@
 		<?php reconasia_get_svg_icons(); ?>
 
 	<header class="header">
-		<div class="header__nav">
+		<nav class="header__nav">
 			<label class="header__toggle" for="toggle">&#9776;</label>
 			<a href="<?php echo get_home_url(); ?>" class="header__logo" title="Go home"><?php include( get_template_directory() . '/assets/static/csisra-logo.svg'); ?></a>
 			<a class="header__search-icon-mobile"><?php echo reconasia_get_svg( 'search' ); ?></a>
 			<input type="checkbox" id="toggle"/>
-			<div class="header__menu">
-				<a href="#">Topics</a>
-				<a href="#">Maps & Data</a>
-				<a href="#">About</a>
-				<!-- site-header -->
-			</div>
+			<ul class="header__menu">
+				<li class="menu-item-has-children">
+					<a>Topics</a>
+					<ul class="sub-menu">
+						<li><a href="">Topic 1</a></li>
+					</ul>
+				</li>
+				</li><a href="#">Maps & Data</a></li>
+				</li><a href="#">About</a></li>
+			</ul>
 			<a class="header__search-icon"><?php echo reconasia_get_svg( 'search' ); ?></a>
-		</div>
-	</header>	
+		</nav>
+	</header>
