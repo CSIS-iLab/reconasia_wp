04ea002ea29987fb3a06c1bd746a1c1325273ead
Lindsay Urchyk <urchykli@gmail.com>
2021-04-08 13:24:09 -0400

    Post featured image source

--- wp-content/themes/reconasia/template-parts/featured-image-caption.php
+++ wp-content/themes/reconasia/template-parts/featured-image-caption.php
@@ -7,19 +7,11 @@
  * @since 1.0.0
  */
 
-$is_singular = is_singular();
-
 if ( has_post_thumbnail() && ! post_password_required() ) {
 
-	// $caption = get_the_post_thumbnail_caption();
+	$caption = get_the_post_thumbnail_caption();
 
-	// $source = get_field('source', get_post_thumbnail_id() );
-	// $sourceHTML = '';
-	// if ( $source ) {
-	// 	$sourceHTML = ' <span class="caption__source">' . esc_html( $source ) . '</span>';
-	// }
-
-	// if ( $caption ) {
-	// 	echo '<div class="featured-media__caption"> ' . esc_html( $caption ) . $sourceHTML . '</div>';
-	// }
+	if ( $caption ) {
+		echo '<div class="featured-media__caption"><span class="featured-media__caption-label">Header Image: </span> ' . esc_html( $caption ) . '</div>';
+	}
 }
