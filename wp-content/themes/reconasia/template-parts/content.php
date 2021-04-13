005e897c380ac8a45f2d3c1836d29b9c00af931d
Lindsay Urchyk <urchykli@gmail.com>
2021-04-13 13:28:21 -0400

    Related post image object fit contain

--- wp-content/themes/reconasia/template-parts/content.php
+++ wp-content/themes/reconasia/template-parts/content.php
@@ -32,7 +32,6 @@
 	<?php if ( is_single() ) { ?>
 	<footer class="single__footer">
 		<?php
-			// get_template_part( 'template-parts/post-related-content' );
 			echo do_shortcode( '[jprel]' );
 			if ( function_exists( 'ADDTOANY_SHARE_SAVE_KIT' ) ) { ADDTOANY_SHARE_SAVE_KIT(); }
 		?>
