b302c375dedfc7cd8ffd055a909bd4bbbfa14cb5
Lindsay Urchyk <urchykli@gmail.com>
2021-04-14 17:51:21 -0400

    Search submit button hover and focus

--- wp-content/themes/reconasia/searchform.php
+++ wp-content/themes/reconasia/searchform.php
@@ -22,7 +22,7 @@ $aria_label = ! empty( $args['label'] ) ? 'aria-label="' . esc_attr( $args['labe
 <form role="search" <?php echo $aria_label; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped -- Escaped above. ?> method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
 	<label for="<?php echo esc_attr( $unique_id ); ?>">
 		<span class="screen-reader-text"><?php _e( 'Search for:', 'reconasia' ); // phpcs:ignore: WordPress.Security.EscapeOutput.UnsafePrintingFunction -- core trusts translations ?></span>
-		<input type="search" id="<?php echo esc_attr( $unique_id ); ?>" class="search-form__input" placeholder="<?php echo esc_attr_x( 'Search', 'placeholder', 'reconasia' ); ?>" value="<?php echo get_search_query(); ?>" name="s" autocomplete="off"/>
+		<input type="search" id="<?php echo esc_attr( $unique_id ); ?>" class="search-form__input" placeholder="<?php echo esc_attr_x( 'Search', 'placeholder', 'reconasia' ); ?>" value="<?php echo get_search_query(); ?>" name="s"/>
 	</label>
-	<input type="submit" class="search-form__submit" value="<?php echo esc_attr_x( 'Search', 'submit button', 'reconasia' ); ?>" autocomplete="off" />
+	<input type="submit" class="search-form__submit" value="<?php echo esc_attr_x( 'Search', 'submit button', 'reconasia' ); ?>" />
 </form>
