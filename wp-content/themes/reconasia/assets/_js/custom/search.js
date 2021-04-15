1bae3a14216fb1be941e04002dba7129688302ae
Lindsay Urchyk <urchykli@gmail.com>
2021-04-15 11:25:06 -0400

    Algolia search results close when search bar is closed

--- wp-content/themes/reconasia/assets/_js/custom/search.js
+++ wp-content/themes/reconasia/assets/_js/custom/search.js
@@ -2,6 +2,7 @@ const Search = () => {
   const trigger = document.querySelector('#search-trigger')
   const searchEl = document.querySelector('#searchform')
   const close = document.querySelector('#search-close')
+  const algolia = document.getElementsByClassName('aa-dropdown-menu')
 
   trigger.addEventListener('click', function () {
     searchEl.classList.add('is-active')
@@ -13,6 +14,7 @@ const Search = () => {
     searchEl.classList.remove('is-active')
     trigger.setAttribute('aria-expanded', false)
     trigger.classList.remove('is-active')
+    algolia[0].style.display = 'none'
   })
 }
 
