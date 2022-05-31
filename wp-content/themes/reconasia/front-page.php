60a8fe902ede3f853e007feb1fc472821e4236b1
Lindsay Urchyk <urchykli@gmail.com>
2022-05-31 14:05:27 -0400

    Added text saying this site is archived

--- wp-content/themes/reconasia/front-page.php
+++ wp-content/themes/reconasia/front-page.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying the home page.
  *
@@ -15,18 +16,18 @@ get_header();
 <main id="site-content" role="main">
 	<?php
 	$excluded_featured_post_ids_from_recent = array();
-	$featured_primary_post = get_field( 'primary_featured_post' );
+	$featured_primary_post = get_field('primary_featured_post');
 
-	if ( $featured_primary_post ) :
+	if ($featured_primary_post) :
 
 		echo '<section class="home__featured-primary">';
 		echo '<h2 class="home__featured-primary-label">Featured</h2>';
 
-		foreach ( $featured_primary_post as $post ) :
+		foreach ($featured_primary_post as $post) :
 			$excluded_featured_post_ids_from_recent[] = $post->ID;
 
-			setup_postdata( $post );
-			get_template_part( 'template-parts/block', get_post_type() );
+			setup_postdata($post);
+			get_template_part('template-parts/block', get_post_type());
 
 		endforeach;
 
@@ -34,23 +35,26 @@ get_header();
 
 		echo '</section>';
 
-	endif; 
+	endif;
 
 
-	$secondary_featured_post = get_field( 'secondary_featured_posts' );
+	$secondary_featured_post = get_field('secondary_featured_posts');
 
-	if ( $secondary_featured_post ) {
+	if ($secondary_featured_post) {
 		foreach ($secondary_featured_post as $post) {
 			$excluded_featured_post_ids_from_recent[] = $post->ID;
 		}
 	}
-	
+
 	?>
 
 	<section class="home__recent">
-		<h2 class="home__recent-label"><?php _e( 'Recent Posts', 'reconasia' ); ?></h2>
+		<div class="home__archived-text">
+			This project is no longer updated. Click <a href="https://www.csis.org/economics">here</a> to follow the CSIS Economics Program's work on global infrastructure.
+		</div>
+		<h2 class="home__recent-label"><?php _e('Recent Posts', 'reconasia'); ?></h2>
 		<?php
-		echo reconasia_get_svg( "3-arrows" );
+		echo reconasia_get_svg("3-arrows");
 
 		echo '<div class="home__recent-posts">';
 		$most_recent_args = array(
@@ -60,12 +64,12 @@ get_header();
 			'post__not_in' => $excluded_featured_post_ids_from_recent
 		);
 
-		$most_recent_posts = new WP_Query( $most_recent_args );
+		$most_recent_posts = new WP_Query($most_recent_args);
 
-		if ( $most_recent_posts->have_posts() ) {
-			while ( $most_recent_posts->have_posts() ) {
+		if ($most_recent_posts->have_posts()) {
+			while ($most_recent_posts->have_posts()) {
 				$most_recent_posts->the_post();
-				get_template_part( 'template-parts/block', get_post_type() );
+				get_template_part('template-parts/block', get_post_type());
 			}
 			wp_reset_postdata();
 		}
@@ -75,15 +79,15 @@ get_header();
 	</section>
 
 	<?php
-	$featured_secondary_posts = get_field( 'secondary_featured_posts' );
-	if ( $featured_secondary_posts ) :
+	$featured_secondary_posts = get_field('secondary_featured_posts');
+	if ($featured_secondary_posts) :
 
 		echo '<section class="home__featured-secondary">';
 
-		foreach ( $featured_secondary_posts as $post ) :
+		foreach ($featured_secondary_posts as $post) :
 
-			setup_postdata( $post );
-			get_template_part( 'template-parts/block', get_post_type() );
+			setup_postdata($post);
+			get_template_part('template-parts/block', get_post_type());
 
 		endforeach;
 
@@ -92,20 +96,20 @@ get_header();
 
 	endif; ?>
 
-<section class="home__cta">
-	<a href="/analysis" class="btn btn--outline-dark btn--med">All Posts <?php echo reconasia_get_svg( "chevron-right" ); ?></a>
-</section>
+	<section class="home__cta">
+		<a href="/analysis" class="btn btn--outline-dark btn--med">All Posts <?php echo reconasia_get_svg("chevron-right"); ?></a>
+	</section>
 
 	<?php
 	$featured_content = get_field('featured_content');
 
-	if ( $featured_content ) :
+	if ($featured_content) :
 		echo '<section class="home__featured-content" style="background-image:url(' . esc_url($featured_content['image']['url']) . ');">';
-			echo '<div class="home__featured-content-wrapper">';
-				echo '<h2 class="home__featured-content-title">' . $featured_content['title'] . '</h2>';
-				echo '<div class="home__featured-content-desc">' . $featured_content['description'] . '</div>';
-				echo '<a class="home__featured-content-link btn btn--outline-light btn--small" href="' . $featured_content['url'] . '">Browse our resources' . reconasia_get_svg( "chevron-right" ) . '</a>';
-			echo '</div>';
+		echo '<div class="home__featured-content-wrapper">';
+		echo '<h2 class="home__featured-content-title">' . $featured_content['title'] . '</h2>';
+		echo '<div class="home__featured-content-desc">' . $featured_content['description'] . '</div>';
+		echo '<a class="home__featured-content-link btn btn--outline-light btn--small" href="' . $featured_content['url'] . '">Browse our resources' . reconasia_get_svg("chevron-right") . '</a>';
+		echo '</div>';
 		echo '</section>';
 	endif;
 
