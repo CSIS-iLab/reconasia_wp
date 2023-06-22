2c0d7512f1f7c5675535e42d2aa1d90542312af9
removedporn <86824510+removedporn@users.noreply.github.com>
2023-06-22 19:50:04 +0800

    Delete wp-content/themes/reconasia directory

--- wp-content/themes/reconasia/assets/_js/custom/bundle.js
+++
@@ -1,9 +0,0 @@
-import { Navigation } from './navbar'
-import { setScrollbarSize } from './scrollbar'
-import Search from './search'
-
-document.addEventListener('DOMContentLoaded', function () {
-  setScrollbarSize()
-  Navigation()
-  Search()
-})
--- wp-content/themes/reconasia/prettier.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  printWidth: 99999,
-  tabWidth: 2,
-  semi: false,
-  singleQuote: true,
-  bracketSpacing: true,
-  useTabs: false,
-}
